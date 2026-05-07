8c2e550d89422b5102cb07b6f14f45a54b6d9e5d
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2026-05-07 11:56:09 +0530

    d

--- lib/seo/metadata.ts
+++ lib/seo/metadata.ts
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
 import type { SeoConfig } from "@/lib/cms/types";
 
-type OpenGraphType = NonNullable<
-  NonNullable<Metadata["openGraph"]> extends { type?: infer T } ? T : never
->;
+type OpenGraphType =
+  | "website"
+  | "article"
+  | "book"
+  | "profile"
+  | "music.song"
+  | "music.album"
+  | "music.playlist"
+  | "music.radio_station"
+  | "video.movie"
+  | "video.episode"
+  | "video.tv_show"
+  | "video.other";
 
 const OPEN_GRAPH_TYPES = [
   "website",
